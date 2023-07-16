59160dd1be9c2b855a9bd591d60f76d1f662c389
Eden Harris <aussiedwarf@gmail.com>
2023-07-16 19:19:07 +1000

    Fixes to cargo.toml and start windows graphics api from args

--- examples/window/src/main.rs
+++ examples/window/src/main.rs
@@ -1,21 +1,28 @@
-/*
-Conditional compiliation
-https://bitshifter.github.io/2020/05/07/conditional-compilation-in-rust/
-*/
 
 use rad::gpu;
-// mod app;
-// mod gpu;
-// mod gui;
+use clap::Parser;
 
 mod main_window;
 
-
-//use nfd2::Response;
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+  #[arg(short, long, default_value_t = ("opengl".to_string()))]
+  graphics_api: String,
+}
 
 fn main() {
+  let args = Args::parse();
 
-  let mut main_window = match main_window::MainWindow::new(gpu::renderer_types::RendererType::DirectX){
+  let graphics_api = match args.graphics_api.as_str() {
+    "opengl" => gpu::renderer_types::RendererType::OpenGL,
+    "directx" => gpu::renderer_types::RendererType::DirectX,
+    "vulkan" => gpu::renderer_types::RendererType::Vulkan,
+    "metal" => gpu::renderer_types::RendererType::Metal,
+    _ => gpu::renderer_types::RendererType::OpenGL
+  };
+
+  let mut main_window = match main_window::MainWindow::new(graphics_api){
     Ok(res) => res,
     Err(_res) => {
       eprintln!("Error");
